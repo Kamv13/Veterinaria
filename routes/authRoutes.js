033409301013a4bf475e621f8d0da2c1c9d90bfb
Kevin Mendoza <ksmv233@gmail.com>
2025-09-22 17:17:00 -0600

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,7 +8,7 @@ router.post('/register', register);
 // Ruta para iniciar sesión
 router.post('/login', login);
 
-// Ruta protegida que requiere token
+// Ruta protegida que requiere token prueba
 router.get('/protected', verifyToken, (req, res) => {
   res.json({ message: `Hola ${req.user.nombre}, accediste a una ruta protegida!` });
 });
