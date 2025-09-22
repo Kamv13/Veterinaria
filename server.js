b9dae25153b0fc185463ab9c61c5a4042ad4c91d
Kevin Mendoza <ksmv233@gmail.com>
2025-09-22 02:03:10 -0600

    pequeño cambio

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ app.use('/api', require('./routes/ventaRoutes'));
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
+  console.log(` Servidor corriendo en el puerto ${PORT}`);
 });
